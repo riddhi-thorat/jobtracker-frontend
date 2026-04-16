06d1d304811bb521066bf949bbd6d58d3ff5fd24
riddhi-thorat <riddhi.thorat@wynum.com>
2026-04-16 11:41:08 +0530

    Use environment variable for API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
 });
 
-// Automatically attach JWT token to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
